4161007be6fec55a55fb0715ea46d1d2bd1892b5
Hamzeh Asadi <hamzeasadi1@gmail.com>
2024-06-01 08:24:34 +0100

    create package and update git ignore

---
+++ setup.py
@@ -0,0 +1,34 @@
+"""
+Setup for camera source package
+
+"""
+
+import os
+from typing import List
+from setuptools import setup
+from setuptools import find_packages
+
+
+
+NAME:str = "camerasource"
+AUTHOR:str = "Hamzeh"
+VERSION:str = "0.1.0"
+LICENSE:str = "MIT"
+DESCRIPTION:str = "deep learning model training pipeline"
+LONG_DESCRIPTION:str = open(os.path.join(os.getcwd(), "README.md"), 
+                            mode="r", encoding="utf-8").read()
+EMAIL:str = "hamzeasadi1@gmail.com"
+REQUIER_PACKAGE:List = ["torch", "einops"]
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    package_data=find_packages(),
+    license=LICENSE,
+    requires=REQUIER_PACKAGE
+    )
